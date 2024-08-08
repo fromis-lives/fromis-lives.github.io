68ff9be1fc33e92af6b85148cac8342b8f192010
fromis-lives <jorgelopezoncall@gmail.com>
2024-08-08 15:34:44 +0200

    Initial commit

---
+++ js/chart.js
@@ -0,0 +1,272 @@
+const memberNames = ["saerom", "hayoung", "gyuri", "jiwon", "jisun", "seoyeon", "chaeyoung", "nagyung", "jiheon"];
+const memberColors = ["green", "beige", "white", "purple", "brown", "red", "blue", "pink", "gray", "orange", "yellow"];
+
+var chart = null;
+var selectedPath = null;
+var memberToValue = null;
+var hapbangValue = null;
+var multipleValue = null;
+
+function getViewDiv(elemData, divClass) {
+    var text = "<div class='" + divClass + "'>"
+    if (elemData["thumbnail"]) {
+        tPath = elemData["thumbnail"][0];
+        tWidth = elemData["thumbnail"][1];
+        tHeight = elemData["thumbnail"][2];
+        text += "<div align='center'><img src='" + tPath + "' ";
+        text += "width='" + tWidth + "' height='" + tHeight + "'/></div>";
+    }
+    text += "<div class='fields_container'>";
+    var firstField = true;
+    for (const [j, [key, value]] of Object.entries(Object.entries(elemData))) {
+        if (key == "thumbnail" || key == "account" || key == "titlekr") {
+            continue;
+        }
+        if (locale == "kr" && (key == "subbed" || key == "sublink")) {
+            continue;
+        }
+        if (firstField) {
+            firstField = false;
+        } else {
+            text += "<br>";
+        }
+        i18key = "field_" + key.replace(/ /g,"_");
+        cKey = key.charAt(0).toUpperCase() + key.slice(1);
+        i18val = translateKey(i18key);
+        keySpan = '<span data-i18n-key="' + i18key + '">' + i18val + "</span>";
+        text += keySpan + "<span>: </span>";
+        if (key == "title") {
+            if (locale == "kr" && elemData["titlekr"]) {
+                text += '<span>' + elemData["titlekr"] + '</span>';
+            } else {
+                text += '<span>' + value + '</span>';
+            }
+        } else if (key == "link" || key == "sublink") {
+            var hostname = new URL(value).hostname;
+            hostname = hostname.replace('www.', '');
+            hostname = hostname.replace('.com', '');
+            hostname = hostname.replace('.io', '');
+            hostname = hostname.charAt(0).toUpperCase() + hostname.slice(1);
+            
+            text += "<a class='out_link' target='_blank' href='" + value + "'>" + hostname + "</a>";
+        } else if (key == "members" || key == "people") {
+            for (let i = 0; i < value.length; ++i) {
+                if (i > 0) {
+                    text += "<span>, </span>";
+                }
+                text += '<span data-i18n-key="' + value[i] + '">' + translateKey(value[i]) + "</span>";
+            }
+        } else if (key == "platform") {
+            text += '<span data-i18n-key="' + value + '">' + translateKey(value) + "</span>";
+            if (elemData["account"]) {
+                const account = elemData["account"];
+                text += "<span> (</span>";
+                text += '<span data-i18n-key="' + account + '">' + translateKey(account) + "</span><span>)</span>";
+            }
+        } else {
+            text += '<span>' + value + '</span>';
+        }
+    }
+    text += "</div></div>"
+    return text;
+}
+
+function buildChart() {
+    if (chart) {
+        return;
+    }
+    
+    memberToValue = {};
+    for (let i = 0; i < memberNames.length; ++i) {
+        memberToValue[memberNames[i]] = (1 << i);
+    }
+    hapbangValue = (1 << (memberNames.length - 1)) + 1;
+    multipleValue = hapbangValue + 1;
+    
+    var rangesArray = [];
+    var colorsArray = [];
+    for (let i = 0; i < memberNames.length; i++) {
+        rangesArray.push({equal: memberToValue[memberNames[i]]});
+    }
+    rangesArray.push({equal: hapbangValue});
+    rangesArray.push({equal: multipleValue});
+    colorsArray = memberColors;
+    const customColorScale = anychart.scales.ordinalColor();
+    customColorScale.ranges(rangesArray);
+    customColorScale.colors(colorsArray);
+    
+    chart = anychart.calendar();
+    chart.contextMenu(false);
+    chart.background("#22282D");
+    chart.colorScale(customColorScale);
+    chart.colorRange(false)
+    
+    chart.months()
+        .stroke("#474747")
+        .noDataStroke("#474747");
+    chart.days()
+        .spacing(4)
+        .stroke(false)
+        .noDataStroke(false)
+        .noDataFill("#2d333b")
+        .noDataHatchFill(false)
+        .hovered()
+            .fill({color: "black", opacity: 0.3})
+            .stroke({color: '#dfdfdf', thickness: 2});
+  
+    const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    if (isMobile) {
+        chart.tooltip(false);
+    } else {
+        chart.tooltip().allowLeaveStage(false).position("left-top").useHtml(true).format(function() {
+            var text = "";
+            text += "<div class='info_tooltip'>";
+            for (let i = 0; i < this.getData("elems").length; i++) {
+                if (i > 0) {
+                    text += "<hr noshade='true' class='anychart-tooltip-separator'>"
+                }
+                text += getViewDiv(this.getData("elems")[i], "tooltip_elem")
+            }
+            text += "</div>"
+            return text;
+        });
+    }
+    
+    chart.listen("pointClick", function(e) {
+        if (selectedPath != null) {
+            selectedPath.setAttribute("stroke", "none");
+        }
+        const row = chart.data().row(e.dataIndex);
+        const elemId = e.originalEvent.getOriginalEvent().target.he
+        selectedPath = document.getElementById(elemId);
+        selectedPath.setAttribute("stroke", "#dfdfdf");
+        selectedPath.setAttribute("stroke-width", "2");
+        
+        dateString = row['x'];
+        var elemDate = document.getElementById("elem_date");
+        elemDate.innerHTML = dateString;
+      
+        const elems = row["elems"];
+        var htmlContent = '';
+        for (let i = 0; i < elems.length; i++) {
+            htmlContent += getViewDiv(elems[i], "info_elem");
+        }
+        document.getElementById("sep_before_info").style.visibility = 'visible';
+        document.getElementById("elem_info_container").innerHTML = htmlContent;
+    });
+
+    chart.listen("chartDraw", function() {
+        document.getElementById("container").style.height = (chart.getActualHeight() + 20) + "px";
+        credits = document.querySelector(".anychart-credits");
+        if (credits) {
+            credits.remove();
+        }
+    });
+    chart.container("container");
+}
+
+function updateChart() {
+    if (selectedPath != null) {
+        selectedPath.setAttribute("stroke", "none");
+        selectedPath = null;
+    }
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const includeTwitchData = urlParams.get('dataTwitch');
+    
+    const memberCheckboxes = document.querySelectorAll('.member_check:checked');
+    const selectedMembers = [...memberCheckboxes].map(e => (e.getAttribute('value').toLowerCase()));
+    const membershipCheckboxes = document.querySelectorAll('.membership_check:checked');
+    const selectedMembership = [...membershipCheckboxes].map(e => (e.getAttribute('value')));
+    const subsCheckboxes = document.querySelectorAll('.subs_check:checked');
+    const selectedSubs = [...subsCheckboxes].map(e => (e.getAttribute('value')));
+    
+    var data = [];
+    for (const [date, row] of Object.entries(fullData)) {
+        var goodElems = row.filter(elem => {
+            if (elem["subbed"] == "Yes") {
+                if (!selectedSubs.includes("Yes")) {
+                    return false;
+                }
+            } else {
+                if (!selectedSubs.includes("No")) {
+                    return false;
+                }
+            }
+            if (elem["membership"] == "Yes") {
+                if (!selectedMembership.includes("Yes")) {
+                    return false;
+                }
+            } else {
+                if (!selectedMembership.includes("No")) {
+                    return false;
+                }
+            }
+            if (selectedMembers.length > 0 && elem["members"].includes("all")) {
+                if (selectedMembers.length > 1 || selectedMembers[0] != "gyuri" || date < "2022-07-31") {
+                    return true;
+                }
+            }
+            for (let mem of elem["members"]) {
+                if (selectedMembers.includes(mem)) {
+                    return true;
+                }
+            }
+            return false;
+        });
+        if (goodElems.length == 0) {
+            continue;
+        }
+        var rowValue = 0;
+        if (goodElems.length > 1) {
+            rowValue = multipleValue;
+        } else if (goodElems[0]["members"].length > 1 || goodElems[0]["people"] || goodElems[0]["members"][0] == "all") {
+            rowValue = hapbangValue;
+        } else {
+            rowValue = memberToValue[goodElems[0]["members"][0]];
+        }
+        data.push({
+            x: date,
+            value: rowValue,
+            elems: goodElems,
+        });
+    }
+    
+    chart.data(data);
+    chart.draw();
+    
+    document.getElementById("elem_date").innerHTML = '';
+    document.getElementById("elem_info_container").innerHTML = '';
+    document.getElementById("sep_before_info").style.visibility = 'hidden';
+}
+
+function modifyBoxIfNeeded(urlParams, field) {
+    const checkStatus = urlParams.get(field);
+    if (checkStatus != null) {
+        document.getElementById(field + '_box').checked = (checkStatus === 'true');
+    }
+}
+
+anychart.onDocumentReady(function () {
+    initializeUserLocale();
+    document.getElementById("last_updated").innerHTML = updateDate;
+    
+    const titleLetters = document.querySelectorAll('.title_letter');
+    for (let i = 0; i < titleLetters.length; ++i) {
+        titleLetters[i].style.setProperty('color', memberColors[i]);
+    }
+    const memberCheckboxes = document.querySelectorAll('.member_check');
+    for (let i = 0; i < memberCheckboxes.length; ++i) {
+        memberCheckboxes[i].style.setProperty('accent-color', memberColors[i]);
+    }
+    
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    modifyBoxIfNeeded(urlParams, 'link_public');
+    modifyBoxIfNeeded(urlParams, 'link_private');
+    modifyBoxIfNeeded(urlParams, 'link_missing');
+    
+    buildChart();
+    updateChart();
+    document.getElementsByTagName("html")[0].style.visibility = "visible";
+});
---
+++ js/i18.js
@@ -0,0 +1,127 @@
+var locale = "kr";
+
+const supportedLocales = ["en", "kr"];
+
+const translations = {
+    "en": {
+        "last_updated": "Last updated",
+        "members": "Members",
+        "saerom": "Saerom",
+        "hayoung": "Hayoung",
+        "gyuri": "Gyuri",
+        "jiwon": "Jiwon",
+        "jisun": "Jisun",
+        "seoyeon": "Seoyeon",
+        "chaeyoung": "Chaeyoung",
+        "nagyung": "Nagyung",
+        "jiheon": "Jiheon",
+        "all": "All",
+        "hapbang": "Hapbang/ft.",
+        "multiple": "Multiple",
+        "membership_filter": "Membership",
+        "membership_yes": "Yes",
+        "membership_no": "No",
+        "subs_filter": "Subs",
+        "field_title": "Title",
+        "field_members": "Members",
+        "field_people": "People",
+        "field_start_time": "Start time",
+        "field_duration": "Duration",
+        "field_platform": "Platform",
+        "field_sublink": "Subbed link",
+        "field_link": "Link",
+        "field_subbed": "Subbed",
+        "field_membership": "Membership",
+        "true": "Yes",
+        "false": "No",
+        "weverse": "Weverse",
+        "vlive": "VLIVE",
+        "youtube": "YouTube",
+        "instagram": "Instagram",
+        "facebook": "Facebook",
+        "twitter": "Twitter",
+        "kakaotv": "KakaoTV",
+        "showroom": "SHOWROOM",
+    },
+    "kr": {
+        "last_updated": "최근 수정일",
+        "members": "멤버",
+        "saerom": "이새롬",
+        "hayoung": "송하영",
+        "gyuri": "장규리",
+        "jiwon": "박지원",
+        "jisun": "노지선",
+        "seoyeon": "이서연",
+        "chaeyoung": "이채영",
+        "nagyung": "이나경",
+        "jiheon": "백지헌",
+        "all": "정원",
+        "hapbang": '합방/ft.',
+        "multiple": "Multiple",
+        "membership_filter": "멤버십",
+        "membership_yes": "Yes",
+        "membership_no": "No",
+        "subs_filter": "Subs",
+        "field_title": "제목",
+        "field_members": "멤버",
+        "field_people": "출연",
+        "field_start_time": "시간",
+        "field_duration": "기간",
+        "field_platform": "플랫폼",
+        "field_link": "링크",
+        "field_membership": "멤버십",
+        "weverse": "위버스",
+        "vlive": "븨앱",
+        "youtube": "유튜브",
+        "instagram": "인스타그램",
+        "facebook": "페이스북",
+        "twitter": "트위터",
+        "kakaotv": "카카오티비",
+        "showroom": "슈룸"
+    }
+};
+
+function translateKey(key) {
+    value = translations[locale][key.toString().toLowerCase()];
+    if (!value) {
+        console.log("Translation not found: " + locale + " - " + key);
+        return key;
+    }
+    return value;
+}
+
+function translateElement(element) {
+    const key = element.getAttribute("data-i18n-key");
+    const translation = translateKey(key);
+    element.innerText = translation;
+}
+
+function translatePage() {
+    document.querySelectorAll("[data-i18n-key]").forEach(translateElement);
+}
+
+function changeLocale(loc) {
+    locale = loc;
+    document.querySelector("#lang_select_kr").classList.toggle("lang_select_on", (locale == "kr"));
+    document.querySelector("#lang_select_kr").classList.toggle("lang_select_off", (locale != "kr"));
+    document.querySelector("#lang_select_en").classList.toggle("lang_select_on", (locale == "en"));
+    document.querySelector("#lang_select_en").classList.toggle("lang_select_off", (locale != "en"));
+    translatePage();
+}
+
+function initializeUserLocale() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    var userLocale = urlParams.get('locale');
+    if (!userLocale) {
+        locales = navigator.languages.map((loc) => loc.split("-")[0]);
+        if (locales.length > 0 && supportedLocales.includes(locales[0])) {
+            userLocale = locales[0];
+        }
+    }
+    if (!userLocale) {
+        userLocale = "en";
+    }
+    changeLocale(userLocale);
+}
+
